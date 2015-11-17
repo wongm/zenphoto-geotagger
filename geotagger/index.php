9dbd9659629dd5ed75bad1e167cb8179a7a99f1f
Marcus Wong <marcusw@pageuppeople.com>
2015-11-17 15:33:35 +1100

    Ensure that non-Zenphoto users cannot access page

--- geotagger/index.php
+++ geotagger/index.php
@@ -1,4 +1,14 @@
 <?php
+
+define('OFFSET_PATH', 3);
+require_once(dirname(dirname(dirname(__FILE__))) . '/zp-core/admin-globals.php');
+require_once('functions.php');
+
+if (!zp_loggedin(MANAGE_ALL_ALBUM_RIGHTS)) {
+	header("HTTP/1.0 403 " . gettext("Forbidden"));
+	return;
+}
+
 if (isset($_GET["action"]))
 {
 	drawResults();
